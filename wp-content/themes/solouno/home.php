f6b4a1eb0948f3ca6aba00ec3fc11f4c6dae1697
Naohiro Fukumoto <naohirozrx@yahoo.co.jp>
2023-02-01 00:52:27 +0900

    トップアコーディオン実装

--- wp-content/themes/solouno/home.php
+++ wp-content/themes/solouno/home.php
@@ -8,38 +8,53 @@
   </section>
 
   <div id="aboutus-area">
-    <section class="aboutus-top">
-      <img src="<?php echo get_template_directory_uri(); ?>/images/home-top_p2.jpg" />
-      <div>
-        <h2>SOLO UNOについて</h2>
-        <img src="<?php echo get_template_directory_uri(); ?>/images/dot-mini.svg" />
-      </div>
-    </section>
-    <section class="text-area">
-      <h2><span>CONCEPT</span>キミだけの、たった一つの。</h2>
-      <img src="<?php echo get_template_directory_uri(); ?>/images/dot.svg" class="dot" />
-      <p>イメージする力を伸ばすと想像力や社会性が豊かになります。</p>
-      <p>リモコンを携帯に見立てたり、テーブルの下を秘密基地にしたりと遊びの中で社会性やコミュニケーション能力を高めていきます。</p>
-      <p>イメージしたものが形になるオーダーメイドに触れることは子供の成長を見守るものになると私たちは考えます。</p>
-      <p>ぼくだけ、わたしだけのたった一つの組み合わせを想像してください。<br />そこにはたくさんの笑顔が生まれます。</p>
-    </section>
-    <section class="photo">
-      <div>
-        <img src="<?php echo get_template_directory_uri(); ?>/images/home-top_p3.jpg" class="p3"/>
-        <img src="<?php echo get_template_directory_uri(); ?>/images/home-top_p4.jpg" class="p4"/>
-      </div>
-      <img src="<?php echo get_template_directory_uri(); ?>/images/home-top_p5.jpg" />
-    </section>
-    <section class="about-area">
-      <h2><span>ABOUT US</span>こだわりを、カタチに。</h2>
-      <img src="<?php echo get_template_directory_uri(); ?>/images/dot.svg" class="dot" />
-      <p>せっかく買うなら『気に入ったもの』を、<br />とことん『こだわったもの』をじっくり選んで、大切に長く愛用してほしい。</p>
-      <p>そんな思いで、オーダーメイドのセレクトショップ<br />SOLO UNOはスタートしました。</p>
-      <p>SOLO UNOとは[たったひとつの]というイタリア語。</p>
-      <p>オーダーメイドでSOLO UNOなお気に入りをつくってください。</p>
-      <p>それは、たくさんの想い出のそばに、いつまでも...<br />6年間の相棒もSOLO UNO[たったひとつの]宝物になりますように。</p>
-    </section>
+    <script>
+      $(function() {
+        $('.accordion .more').on('click', function() {
+          $('.accordion').toggleClass('show');
+        });
 
+        $('.accordion2 .more').on('click', function() {
+          $('.accordion2').toggleClass('show');
+        });
+      });
+    </script>
+    <div class="accordion">
+      <div>
+        <section class="text-area">
+          <h2><span>Concept</span>ランドセルに、オーダーメイドという選択を。</h2>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/dot.svg" class="dot" />
+          <p>『想像力』が必要なオーダーメイドには、好みが明確になっている大人向けのアイテムが多く存在します。</p>
+          <p>しかし、私たちは子どもにも”オーダーメイドを選ぶ体験”をしてほしいと思っています。</p>
+          <p>イメージする力を伸ばすと想像力や社会性が豊かになります。</p>
+          <p>リモコンを携帯に見立てたり、テーブルの下を秘密基地にしたりと、<br />遊びの中で社会性やコミュニケーション能力を高めていきます。</p>
+          <p>『イメージしたものが形になるオーダーメイドに触れることは、子供の成長を見守るものになる』<br />と私たちは考えます。</p>
+          <p>ぼくだけ、わたしだけのたった一つの組み合わせを想像してください。<br />そこにはたくさんの笑顔が生まれます。</p>
+        </section>
+        <section class="photo">
+          <div>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/home-top_p3.jpg" class="p3"/>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/home-top_p4.jpg" class="p4"/>
+          </div>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/home-top_p5.jpg" />
+        </section>
+        <button class="more"><img src="<?php echo get_template_directory_uri(); ?>/images/slide-toggle-arrow.svg" /></button>
+      </div>
+    </div>
+    <div class="accordion2">
+      <div>
+        <section class="about-area">
+          <h2><span>ABOUT US</span>こだわりを、カタチに。</h2>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/dot.svg" class="dot" />
+          <p>せっかく買うなら『気に入ったもの』を、<br />とことん『こだわったもの』をじっくり選んで、大切に長く愛用してほしい。</p>
+          <p>そんな思いで、オーダーメイドのセレクトショップ<br />SOLO UNOはスタートしました。</p>
+          <p>SOLO UNOとは[たったひとつの]というイタリア語。</p>
+          <p>オーダーメイドでSOLO UNOなお気に入りをつくってください。</p>
+          <p>それは、たくさんの想い出のそばに、いつまでも...<br />6年間の相棒もSOLO UNO[たったひとつの]宝物になりますように。</p>
+        </section>
+        <button class="more"><img src="<?php echo get_template_directory_uri(); ?>/images/slide-toggle-arrow.svg" /></button>
+      </div>
+    </div>
   </div>
 
   <section class="aboutus">
--- wp-content/themes/solouno/sidebar-side-l.php
+++ wp-content/themes/solouno/sidebar-side-l.php
@@ -2,7 +2,7 @@
   <div class="bg-area">
     <div class="menu-area">
       <span class="sns">
-        SNS<img src="<?php echo get_template_directory_uri(); ?>/images/sns-border.svg" /><img src="<?php echo get_template_directory_uri(); ?>/images/sns-instagram.svg" />
+        SNS<img src="<?php echo get_template_directory_uri(); ?>/images/sns-border.svg" /><a href="https://www.instagram.com/solo_uno_ordermade/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/sns-instagram.svg" /></a>
       </span>
       <h1>
         <a href="<?php echo home_url('/')?>"><script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
@@ -14,9 +14,9 @@
         <li><a href="<?php echo home_url('/')?>aboutus">SOLO UNOについて</a></li>
         <li><a href="<?php echo home_url('/')?>product">PRODUCT</a></li>
         <li><a href="<?php echo home_url('/')?>showroom">店舗情報</a></li>
-        <li><a href="<?php echo home_url('/')?>news">お知らせ</a></li>    
+        <li><a href="<?php echo home_url('/')?>news">お知らせ</a></li>
         <li><a href="<?php echo home_url('/')?>exhibit">展示会情報</a></li>
-        <!-- <li><a href="<?php echo home_url('/')?>column">コラム</a></li> -->    
+        <!-- <li><a href="<?php echo home_url('/')?>column">コラム</a></li> -->
         <li><a href="<?php echo home_url('/')?>request">カタログ請求</a></li>
       </ul>
     </div>
