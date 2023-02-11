25c8ab688188f53bfb6992b03e1fea3c1c42bb6e
Naohiro Fukumoto <naohirozrx@yahoo.co.jp>
2023-02-12 00:00:17 +0900

    プライス追加

--- wp-content/themes/solouno/page-flow.php
+++ wp-content/themes/solouno/page-flow.php
@@ -11,63 +11,13 @@
   <section class="schoolbag-area">
     <nav style="margin-bottom: 40px;">
       <ul style="line-height: 1.3;">
-        <li style="margin-bottom: 0.5em;">〇<a href="#flow" style="text-decoration: underline;">ランドセルができるまで</a></li>
         <li style="margin-bottom: 0.5em;">〇<a href="#step" style="text-decoration: underline;">オーダーの手順</a></li>
-        <li style="margin-bottom: 0.5em;">〇<a href="#spec" style="text-decoration: underline;">SOLO UNOの便利機能</a></li>
+        <li style="margin-bottom: 0.5em;">〇<a href="#spec" style="text-decoration: underline;">PRICE＆SPEC</a></li>
+        <li style="margin-bottom: 0.5em;">〇<a href="#howtoorder" style="text-decoration: underline;">HOW TO ORDER</a></li>
+        <li style="margin-bottom: 0.5em;">〇<a href="#function" style="text-decoration: underline;">SOLO UNOの便利機能</a></li>
         <li style="margin-bottom: 0.5em;">〇<a href="#gallery" style="text-decoration: underline;">GALLERY</a></li>
       </ul>
     </nav>
-    <h2 id="flow"><img src="<?php echo get_template_directory_uri(); ?>/images/dot-mini2.svg" />ランドセルができるまで<img src="<?php echo get_template_directory_uri(); ?>/images/dot-mini2.svg" /></h2>
-    <div>
-      <!--<dl>
-        <dt>
-          <img src="<?php echo get_template_directory_uri(); ?>/images/flow-1.jpg" />組み合わせを選ぶ
-        </dt>
-        <dd>
-          <h3>店舗、展示会にサンプルを見に行く</h3>
-          <p class="text1">クラリーノエフ  64,800円<br />
-          クラリーノタフロックNEO  69,800円</p>
-          <p class="text2">※サイドのデザインに一部追加料金のかかるものがございます。</p>
-          <h3>WEBページからシミュレーションをする</h3>
-        </dd>
-      </dl>-->
-      <dl>
-        <dt>
-          <img src="<?php echo get_template_directory_uri(); ?>/images/flow-2.jpg" />1. お申し込み
-        </dt>
-        <dd>
-          <h3>組み合わせが決まりましたらお申し込みください。<br />
-          ■ WEBサイトのシミュレーションから<br />
-          ■ 店舗/展示会で</h3>
-          <p class="text2">※一部お申し込みを承れない展示会場がございます。<br />※お申し込み時にお支払いください。</p>
-          <h3 style="text-decoration: underline;">お申し込みから２週間は組み合わせの変更が可能です。</h3>
-          <img src="<?php echo get_template_directory_uri(); ?>/images/gentei.svg" />
-        </dd>
-      </dl>
-      <dl>
-        <dt>
-          <img src="<?php echo get_template_directory_uri(); ?>/images/flow-4.jpg" />2. 製作/お届け
-        </dt>
-        <dd>
-          <h3>お申し込みから2週間（組み合わせ確定）後、部材準備、製作を開始。出来上がり次第順次お届けいたします。</h3>
-          <p class="text2">※製作都合上、お申し込み順でのお届けではありません。<br />
-          ※出来上がり時期を早めたり、ご指定いただくことは出来かねます。<br />
-          ※お申し込み後のキャンセル、返金は承れません。ご了承の上お申し込みください。</p>
-          <img src="<?php echo get_template_directory_uri(); ?>/images/yotei.svg" />
-        </dd>
-      </dl>
-      <!--<dl>
-        <dt>
-          <img src="<?php echo get_template_directory_uri(); ?>/images/flow-4.jpg" />お届け
-        </dt>
-        <dd>
-          <h3>2024年1月中旬〜3月中旬ごろ出来上がり次第お届けいたします。</h3>
-          <h3>お申し込み後のキャンセル、返金は承れません。</h3>
-          <img src="<?php echo get_template_directory_uri(); ?>/images/gentei.svg" />
-        </dd>
-      </dl>-->
-      <a href="https://sim.solouno-ordermade.com" class="">オーダーメイドを選んでみよう！<br />【シュミレーター】</a>
-    </div>
   </section>
 
   <section class="order-flow">
@@ -475,29 +425,139 @@
     </div>
   </section>
 
-  <section class="convenience-area">
-    <h2 id="spec"><img src="<?php echo get_template_directory_uri(); ?>/images/dot-mini2.svg" />SOLO UNOの便利機能<img src="<?php echo get_template_directory_uri(); ?>/images/dot-mini2.svg" /></h2>
-    <img src="<?php echo get_template_directory_uri(); ?>/images/flow-convenience.svg" />
+  <section class="spec-area">
+    <h2 id="spec"><img src="<?php echo get_template_directory_uri(); ?>/images/dot-mini2.svg" />ORDER PRICE & SPEC<img src="<?php echo get_template_directory_uri(); ?>/images/dot-mini2.svg" /></h2>
+    <h3>クラリーノ エフ 64,800円</h3>
+    <ul class="price-list">
+      <li>
+        <figure>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/order-1black.png" />
+          <figcaption>ブラック</figcaption>
+        </figure>
+      </li>
+      <li>
+        <figure>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/order-2navy.png" />
+          <figcaption>ネイビー</figcaption>
+        </figure>
+      </li>
+      <li>
+        <figure>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/order-3blue.png" />
+          <figcaption>ブルー</figcaption>
+        </figure>
+      </li>
+      <li>
+        <figure>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/order-4olive.png" />
+          <figcaption>オリーブ</figcaption>
+        </figure>
+      </li>
+      <li>
+        <figure>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/order-5vividpink.png" />
+          <figcaption>ビビッド<br />ピンク</figcaption>
+        </figure>
+      </li>
+      <li>
+        <figure>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/order-9mistyrose.png" />
+          <figcaption>ミスティ<br />ローズ</figcaption>
+        </figure>
+      </li>
+      <li>
+        <figure>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/order-10marshmallowpink.png" />
+          <figcaption>マシュマロ<br />ピンク</figcaption>
+        </figure>
+      </li>
+      <li>
+        <figure>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/order-11iris.png" />
+          <figcaption>アイリス</figcaption>
+        </figure>
+      </li>
+      <li>
+        <figure>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/order-12marshmallowblue.png" />
+          <figcaption>マシュマロ<br />ブルー</figcaption>
+        </figure>
+      </li>
+      <li>
+        <figure>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/order-13mintgreen.png" />
+          <figcaption>ミント<br />グリーン</figcaption>
+        </figure>
+      </li>
+      <li>
+        <figure>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/order-14ivory.png" />
+          <figcaption>アイボリー</figcaption>
+        </figure>
+      </li>
+      <li>
+        <figure>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/order-15cinnamon.png" />
+          <figcaption>シナモン</figcaption>
+        </figure>
+      </li>
+      <li>
+        <figure>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/order-16greige.png" />
+          <figcaption>グレージュ</figcaption>
+        </figure>
+      </li>
+    </ul>
+
+    <h3>クラリーノ タフロックNEO 69,800円</h3>
+    <ul class="price-list2">
+      <li>
+        <figure>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/order-6tnblack.png" />
+          <figcaption>タフネオブラック</figcaption>
+        </figure>
+      </li>
+      <li>
+        <figure>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/order-7tnnavy.png" />
+          <figcaption>タフネオネイビー</figcaption>
+        </figure>
+      </li>
+    </ul>
+
+
+    <h3>サイドのデザイン</h3>
+
+    <ul class="price-list3">
+      <li>
+        <h4>アラベスク / クローバー</h4>
+        <img src="<?php echo get_template_directory_uri(); ?>/images/priceside-1.jpg" />
+        <p>上記の金額に<br />＋3,000円</p>
+      </li>
+      <li>
+        <h4>レース</h4>
+        <img src="<?php echo get_template_directory_uri(); ?>/images/priceside-2.jpg" />
+        <p>上記の金額に<br />＋5,000円</p>
+      </li>
+    </ul>
+
+    <h3>SPEC</h3>
+
     <dl>
-      <dt><img src="<?php echo get_template_directory_uri(); ?>/images/flow-quality1.svg" /></dt>
-      <dd>
-        <p>SOLO UNOのオーダーメイドランドセルは<br />
-        60年の歴史のある老舗国内工場に製作をお願いしています。<br />
-        職人さんの手により一つひとつ丁寧に縫製されます。</p>
-        <p>ランドセルの色、デザイン形はもちろん、形状や機能、安全性に至るまで何度も打ち合わせをし、サンプルを製作してもらいつくりあげたランドセルです。</p>
-        <p>6年間安心してご使用ください。</p>
-      </dd>
+      <dt>背中/ベルト裏</dt>
+      <dd>人工皮革</dd>
     </dl>
     <dl>
-      <dt><img src="<?php echo get_template_directory_uri(); ?>/images/flow-quality2.svg" /></dt>
-      <dd>
-        <p>SOLO UNOのオーダーメイドランドセルには<br />
-        ご入学からご卒業までの6年間の修理保証サービスがございます。</p>
-        <p>万が一、不具合が発生した場合はご連絡ください。国内工場の職人の手により迅速にお直しいたします。</p>
-        <p>糸ほつれや金具の破損などの通常利用時の不具合は無償にてお直しいたします。<br />
-        キズやらくがき、使用による擦れなど故障ではないお直しは、有償にて承らせていただきます。</p>
-        <p>お直し中にご使用いただく代替えランドセルのご用意がございます。無料でお貸出しいたしますが、お戻しいただく際の片道送料のみご負担をお願いいたします。</p>
-      </dd>
+      <dt>内張り</dt>
+      <dd>合成皮革</dd>
+    </dl>
+    <dl>
+      <dt>大マチ内寸</dt>
+      <dd>ヨコ22,5×マチ幅12,5× 高さ31cm</dd>
+    </dl>
+    <dl>
+      <dt>重量</dt>
+      <dd>約1200g</dd>
     </dl>
   </section>
 
@@ -586,6 +646,85 @@
 <path d="M8.49769 19.9832L20.4294 11.1914C20.7776 10.9362 21.2233 10.9362 21.5715 11.1914L33.5032 19.9832C33.8143 20.2151 34 20.6141 34 21.0549V28.7332C34 29.2435 33.7447 29.7028 33.35 29.8977C33.0193 30.0661 32.6496 30.0228 32.3519 29.7956L20.9773 21.1801L9.6398 29.5358C8.8821 30.0989 7.96631 29.3569 8.00095 28.4641C8.00092 28.4641 8.00092 21.0549 8.00092 21.0549C8.00092 20.6141 8.18663 20.2151 8.49769 19.9832Z" fill="#AC7746"/>
 </svg></a>
 </span>
+  </section>
+  <section class="howtoorder">
+  <h2 id="howtoorder"><img src="<?php echo get_template_directory_uri(); ?>/images/dot-mini2.svg" />HOW TO ORDER<img src="<?php echo get_template_directory_uri(); ?>/images/dot-mini2.svg" /></h2>
+    <div>
+      <!--<dl>
+        <dt>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/flow-1.jpg" />組み合わせを選ぶ
+        </dt>
+        <dd>
+          <h3>店舗、展示会にサンプルを見に行く</h3>
+          <p class="text1">クラリーノエフ  64,800円<br />
+          クラリーノタフロックNEO  69,800円</p>
+          <p class="text2">※サイドのデザインに一部追加料金のかかるものがございます。</p>
+          <h3>WEBページからシミュレーションをする</h3>
+        </dd>
+      </dl>-->
+      <dl>
+        <dt>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/flow-2.jpg" />1. お申し込み
+        </dt>
+        <dd>
+          <h3>組み合わせが決まりましたらお申し込みください。<br />
+          ■ WEBサイトのシミュレーションから<br />
+          ■ 店舗/展示会で</h3>
+          <p class="text2">※一部お申し込みを承れない展示会場がございます。<br />※お申し込み時にお支払いください。</p>
+          <h3 style="text-decoration: underline;">お申し込みから２週間は組み合わせの変更が可能です。</h3>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/gentei.svg" />
+        </dd>
+      </dl>
+      <dl>
+        <dt>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/flow-4.jpg" />2. 製作/お届け
+        </dt>
+        <dd>
+          <h3>お申し込みから2週間（組み合わせ確定）後、部材準備、製作を開始。出来上がり次第順次お届けいたします。</h3>
+          <p class="text2">※製作都合上、お申し込み順でのお届けではありません。<br />
+          ※出来上がり時期を早めたり、ご指定いただくことは出来かねます。<br />
+          ※お申し込み後のキャンセル、返金は承れません。ご了承の上お申し込みください。</p>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/yotei.svg" />
+        </dd>
+      </dl>
+      <!--<dl>
+        <dt>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/flow-4.jpg" />お届け
+        </dt>
+        <dd>
+          <h3>2024年1月中旬〜3月中旬ごろ出来上がり次第お届けいたします。</h3>
+          <h3>お申し込み後のキャンセル、返金は承れません。</h3>
+          <img src="<?php echo get_template_directory_uri(); ?>/images/gentei.svg" />
+        </dd>
+      </dl>-->
+      <a href="https://sim.solouno-ordermade.com" class="">オーダーメイドを選んでみよう！<br />【シュミレーター】</a>
+    </div>
+  </section>
+
+  <section class="convenience-area">
+    <h2 id="function"><img src="<?php echo get_template_directory_uri(); ?>/images/dot-mini2.svg" />SOLO UNOの便利機能<img src="<?php echo get_template_directory_uri(); ?>/images/dot-mini2.svg" /></h2>
+    <img src="<?php echo get_template_directory_uri(); ?>/images/flow-convenience.svg" />
+    <dl>
+      <dt><img src="<?php echo get_template_directory_uri(); ?>/images/flow-quality1.svg" /></dt>
+      <dd>
+        <p>SOLO UNOのオーダーメイドランドセルは<br />
+        60年の歴史のある老舗国内工場に製作をお願いしています。<br />
+        職人さんの手により一つひとつ丁寧に縫製されます。</p>
+        <p>ランドセルの色、デザイン形はもちろん、形状や機能、安全性に至るまで何度も打ち合わせをし、サンプルを製作してもらいつくりあげたランドセルです。</p>
+        <p>6年間安心してご使用ください。</p>
+      </dd>
+    </dl>
+    <dl>
+      <dt><img src="<?php echo get_template_directory_uri(); ?>/images/flow-quality2.svg" /></dt>
+      <dd>
+        <p>SOLO UNOのオーダーメイドランドセルには<br />
+        ご入学からご卒業までの6年間の修理保証サービスがございます。</p>
+        <p>万が一、不具合が発生した場合はご連絡ください。国内工場の職人の手により迅速にお直しいたします。</p>
+        <p>糸ほつれや金具の破損などの通常利用時の不具合は無償にてお直しいたします。<br />
+        キズやらくがき、使用による擦れなど故障ではないお直しは、有償にて承らせていただきます。</p>
+        <p>お直し中にご使用いただく代替えランドセルのご用意がございます。無料でお貸出しいたしますが、お戻しいただく際の片道送料のみご負担をお願いいたします。</p>
+      </dd>
+    </dl>
   </section>
 </div>
 <script>
