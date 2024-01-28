6fea9396d9ded0affc10387c67b473774684d275
Naohiro Fukumoto <naohirozrx@yahoo.co.jp>
2024-01-28 14:26:46 +0900

    通常価格表記

--- wp-content/themes/simple-plus_child/wc_templates/wc_item_single_custombag.php
+++ wp-content/themes/simple-plus_child/wc_templates/wc_item_single_custombag.php
@@ -212,6 +212,7 @@ $welcart_simpleplus_primary_class = isset( $args ) && isset( $args['column'] ) ?
 								<?php if( usces_the_itemCprice('return') != usces_the_itemPrice('return') ){
 									echo '<span>通常価格：'.usces_the_itemCpriceCr().'</span>';
 								} ?>
+								<?php usces_the_itemCpriceCr(); ?>
 								<?php usces_the_itemPriceCr(); ?><?php usces_guid_tax(); ?>
 								<?php usces_crform_the_itemPriceCr_taxincluded(); ?>
 							</div>
