a4941051e3bff4c4e5db2d80dfd954ba5b9810d4
Naohiro Fukumoto <naohirozrx@yahoo.co.jp>
2024-02-03 00:30:43 +0900

    店舗情報追加

--- wp-content/themes/simple-plus_child/page-showroom.php
+++ wp-content/themes/simple-plus_child/page-showroom.php
@@ -10,7 +10,6 @@
   </section>
   <section class="showroom-grandopen">
     <h2>SHOWROOM</h2>
-    <p>2023.2.10 GRAND OPEN</p>
     <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/showroom-img.jpg" />
     <div>
       <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/map.png" />
@@ -21,6 +20,46 @@
       <address>〒330-0854<br />埼玉県さいたま市大宮区桜木町1-9-1 三谷ビル1階</address>
       <span class="tell"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tell.svg" />048-658-3900</span>
       <a href="https://goo.gl/maps/eoRcmGMziU8eKetU8" target="_blank">Google Mapsを開く</a>
+    </div>
+  </section>
+
+  <section class="showroom-grandopen">
+    <h2><span>SOLO UNO姉妹店</span>シブヤ大阪ショールーム</h2>
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/shibuya-osaka-img.webp" />
+    <div>
+
+      <span class="moyori">阪急淡路駅　西改札口より徒歩3分。<br />
+      淡路商店街、ドトールコーヒーのとなりのビル2階<br />
+      <br />
+      ※第1駐車場/1台分　第2駐車場/2台分</span>
+      <span class="open-close">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/clock.svg" />4月～12月　土・日・祝のみ営業　 10:00～17:00
+      </span>
+      <address>〒533-0032<br />大阪府大阪市東淀川区淡路4-14-15　シブヤビル2F</address>
+      <span class="tell"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tell.svg" />06-6326-0300</span>
+      <a href="https://maps.app.goo.gl/6XpGHN4oHbjqaP4W8" target="_blank">Google Mapsを開く</a>
+    </div>
+  </section>
+
+  <section class="showroom-grandopen">
+    <h2><span>SOLO UNO姉妹店</span>シブヤランドセル名古屋店</h2>
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/shibuya-nagoya-img.jpg" />
+    <div>
+
+      <span class="moyori">名古屋市営地下鉄桜通線・徳重駅より徒歩5分<br />
+      神沢駅より徒歩3分<br />
+      <br />
+      ※駐車場/19台分</span>
+      <span class="open-close">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/clock.svg" />
+        <span>
+          <span style="display:block; margin-bottom:4px; line-height:1.3;">■4月～9月<br />月・金・土・日・祝のみ営業　10:00～17:00</span>
+          <span style="line-height:1.3;">■10月～3月<br />土・日・祝のみ営業　10:00～17:00</span>
+        </span>
+      </span>
+      <address>〒458-0003<br />愛知県名古屋市緑区黒沢台4-1511鳴海プラザ1F D-1</address>
+      <span class="tell"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tell.svg" />052-879-3880</span>
+      <a href="https://maps.app.goo.gl/S2neYdb211bC3pLKA" target="_blank">Google Mapsを開く</a>
     </div>
   </section>
 </div>
