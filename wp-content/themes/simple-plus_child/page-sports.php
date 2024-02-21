e95f5116ed801e7f0b140b1a629d627404124c54
Naohiro Fukumoto <naohirozrx@yahoo.co.jp>
2024-02-21 14:08:19 +0900

    スポーツランドセル追加

--- wp-content/themes/simple-plus_child/page-sports.php
+++ wp-content/themes/simple-plus_child/page-sports.php
@@ -2,18 +2,422 @@
 
 <div id="sports-area">
   <section class="sports-top">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports.jpg" alt="スポーツブランドランドセル" />
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/sports-title.png" alt="スポーツブランドランドセル" />
   </section>
   <section class="sports-contents">
-    <h2>スポーツブランドランドセル</h2>
-    <p>現在スポーツブランドランドセルページは準備中です。<br />
-    <br />
-WEBサイトへの商品掲載2月上旬、<br />
-販売開始は3月上旬頃を予定しています。<br />
-<br />
-店舗では2月1日より一部アイテムの展示を開始し、3月上旬には揃う予定です。<br />
-展示状況はお電話にてお問い合わせください。
-</p>
+    <ul class="tab-menu">
+      <li class="active"><a href="#tab1"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/menu-adidas.svg" alt="アディダス" /></a></li>
+      <li><a href="#tab2"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/menu-nike.svg" alt="NIKE" /></a></li>
+      <li><a href="#tab3"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/menu-puma.svg" alt="PUMA" /></a></li>
+    </ul>
+    <div class="tab-content">
+      <div id="tab1" class="tab-item active">
+        <section class="contents-white">
+          <h2>adidas advance<span>(アディダス アドバンス)</span></h2>
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/dot-mini.svg" class="dot" />
+          <p>シャープなカブセラインが特徴のNEWモデル<br />
+          ブラックのベースにアクセントカラーのロゴが映える</p>
+          <span class="price">79,200円</span>
+          <figure>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/adidas-advance-01.png" />
+          </figure>
+          <h3>カラーバリエーション</h3>
+
+          <ul class="color-vali">
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/adidas-advance-color-1.svg" />
+              <span>①ブラック×ブラック</span>
+            </li>
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/adidas-advance-color-2.svg" />
+              <span>②ブラック×イエロー</span>
+            </li>
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/adidas-advance-color-3.svg" />
+              <span>③ブラック×レッド</span>
+            </li>
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/adidas-advance-color-4.svg" />
+              <span>④ブラック×ブルー</span>
+            </li>
+          </ul>
+
+          <div class="spec">
+            <dl>
+              <dt>主素材</dt>
+              <dd>タフガード<small>®</small>ECOPET<small>®</small></dd>
+            </dl>
+            <dl>
+              <dt>サイズ(内寸)</dt>
+              <dd>約W23.5×H32×D12.5cm</dd>
+            </dl>
+            <dl>
+              <dt>重量</dt>
+              <dd>約1,250g</dd>
+            </dl>
+          </div>
+        </section>
+        <section class="contents-brown">
+          <h2>adidas<span>ランドセル キューブ型 </span></h2>
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/dot-mini.svg" class="dot" />
+          <p>アディダスロゴと3ストライプのゴールドの反射プリントがポイントの軽くて機能的なキューブ型ランドセル</p>
+          <span class="price">84,700円</span>
+          <div class="slider-wrap">
+            <div id="slider">
+              <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/adidas-cube-01.png" alt="adidas" /></div>
+              <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/adidas-cube-02.png" alt="adidas" /></div>
+              <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/adidas-cube-03.png" alt="adidas" /></div>
+              <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/adidas-cube-04.png" alt="adidas" /></div>
+            </div>
+
+            <div id="bx-pager" class="slider-thumb">
+              <a data-slide-index="0" href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/adidas-cube-01.png" alt="adidas" /></a>
+              <a data-slide-index="1" href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/adidas-cube-02.png" alt="adidas" /></a>
+              <a data-slide-index="2" href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/adidas-cube-03.png" alt="adidas" /></a>
+              <a data-slide-index="3" href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/adidas-cube-04.png" alt="adidas" /></a>
+            </div>
+          </div>
+
+          <h3>カラーバリエーション</h3>
+
+          <ul class="color-vali">
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/adidas-advance-color-2.svg" />
+              <span>①ブラック×レッド</span>
+            </li>
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/adidas-advance-color-4.svg" />
+              <span>②ブラック×ブルー</span>
+            </li>
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/adidas-advance-color-3.svg" />
+              <span>③ブラック×イエロー</span>
+            </li>
+          </ul>
+
+          <div class="spec">
+            <dl>
+              <dt>主素材</dt>
+              <dd>クラリーノ<small>®</small>エフ</dd>
+            </dl>
+            <dl>
+              <dt>サイズ(内寸)</dt>
+              <dd>約W23.5×H31×D12.5cm</dd>
+            </dl>
+            <dl>
+              <dt>重量</dt>
+              <dd>約1,160g</dd>
+            </dl>
+          </div>
+        </section>
+
+
+      </div>
+      <div id="tab2" class="tab-item">
+        <section class="contents-white">
+          <h2>NIKEランドセル<span>FD1061C</span></h2>
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/dot-mini.svg" class="dot" />
+          <p>目を引くカラーリングとNIKEロゴがあざやかなスポーティなランドセル</p>
+          <span class="price">88,000円</span>
+
+          <div class="slider-wrap">
+            <div id="slider2">
+              <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike-1601-01.png" alt="nike" /></div>
+              <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike-1601-02.png" alt="nike" /></div>
+              <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike-1601-03.png" alt="nike" /></div>
+              <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike-1601-04.png" alt="nike" /></div>
+            </div>
+
+            <div id="bx-pager2" class="slider-thumb">
+              <a data-slide-index="0" href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike-1601-01.png" alt="nike" /></a>
+              <a data-slide-index="1" href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike-1601-02.png" alt="nike" /></a>
+              <a data-slide-index="2" href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike-1601-03.png" alt="nike" /></a>
+              <a data-slide-index="3" href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike-1601-04.png" alt="nike" /></a>
+            </div>
+          </div>
+
+          <h3>カラーバリエーション</h3>
+
+          <ul class="color-vali">
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/puma1-color-1.svg" />
+              <span>①マリンブルー×ブラック</span>
+            </li>
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/puma1-color-2.svg" />
+              <span>②カーマインレッド×ブラック</span>
+            </li>
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/puma1-color-3.svg" />
+              <span>③アッシュブルー×ブラック</span>
+            </li>
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/puma1-color-4.svg" />
+              <span>④ブラック×パールメタルレッド</span>
+            </li>
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/puma1-color-5.svg" />
+              <span>⑤ブラック×パールメタルマリン</span>
+            </li>
+          </ul>
+          </ul>
+
+          <div class="spec">
+            <dl>
+              <dt>主素材</dt>
+              <dd>ベルエース</dd>
+            </dl>
+            <dl>
+              <dt>サイズ(内寸)</dt>
+              <dd>約W23.5×H30×D12cm</dd>
+            </dl>
+            <dl>
+              <dt>重量</dt>
+              <dd>約1,300g</dd>
+            </dl>
+          </div>
+        </section>
+        
+        <section class="contents-brown">
+          <h2>NIKEランドセル<span>GC20251</span></h2>
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/dot-mini.svg" class="dot" />
+          <p>控えめなロゴとシンプルな単色カラーが上品なNEWモデル</p>
+          <span class="price">93,500円</span>
+
+          <div class="slider-wrap">
+            <div id="slider3">
+              <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike20251-01.png" alt="nike" /></div>
+              <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike20251-02.png" alt="nike" /></div>
+              <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike20251-03.png" alt="nike" /></div>
+              <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike20251-04.png" alt="nike" /></div>
+            </div>
+
+            <div id="bx-pager3" class="slider-thumb">
+              <a data-slide-index="0" href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike20251-01.png" alt="nike" /></a>
+              <a data-slide-index="1" href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike20251-02.png" alt="nike" /></a>
+              <a data-slide-index="2" href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike20251-03.png" alt="nike" /></a>
+              <a data-slide-index="3" href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike20251-04.png" alt="nike" /></a>
+            </div>
+          </div>
+
+          <h3>カラーバリエーション</h3>
+
+          <ul class="color-vali">
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike-2-color-01.svg" />
+              <span>①ブラック</span>
+            </li>
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike-2-color-02.svg" />
+              <span>②ネイビー</span>
+            </li>
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike-2-color-03.svg" />
+              <span>③シルバー</span>
+            </li>
+          </ul>
+
+          <div class="spec">
+            <dl>
+              <dt>主素材</dt>
+              <dd>ベルエース</dd>
+            </dl>
+            <dl>
+              <dt>サイズ(内寸)</dt>
+              <dd>約W23.5×H30×D12cm</dd>
+            </dl>
+            <dl>
+              <dt>重量</dt>
+              <dd>約1,300g</dd>
+            </dl>
+          </div>
+        </section>
+      </div>
+      <div id="tab3" class="tab-item">
+        <section class="contents-white">
+          <h2>PUMAスピードスター</h2>
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/dot-mini.svg" class="dot" />
+          <p>大胆な色使いとビッグなPUMAロゴでスポーティさを最大限に押し出したデザイン</p>
+          <span class="price">①②③ 73,700円</span>
+          <span class="price">④⑤　 78,100円</span>
+
+          <div class="slider-wrap">
+            <div id="slider4">
+              <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/puma1-img-01.png" alt="puma" /></div>
+              <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/puma1-img-02.png" alt="puma" /></div>
+              <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/puma1-img-03.png" alt="puma" /></div>
+              <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/puma1-img-04.png" alt="puma" /></div>
+            </div>
+
+            <div id="bx-pager4" class="slider-thumb">
+              <a data-slide-index="0" href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/puma1-img-01.png" alt="puma" /></a>
+              <a data-slide-index="1" href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/puma1-img-02.png" alt="puma" /></a>
+              <a data-slide-index="2" href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/puma1-img-03.png" alt="puma" /></a>
+              <a data-slide-index="3" href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/puma1-img-04.png" alt="puma" /></a>
+            </div>
+          </div>
+
+          <h3>カラーバリエーション</h3>
+
+          <ul class="color-vali">
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike-1601-color-01.svg" />
+              <span>①ブラック×ブルー</span>
+            </li>
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike-1601-color-02.svg" />
+              <span>②ブラック×レッド</span>
+            </li>
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike-1601-color-03.svg" />
+              <span>③ブラック×シルバー</span>
+            </li>
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike-1601-color-04.svg" />
+              <span>④シルバー×ブラック</span>
+            </li>
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike-1601-color-05.svg" />
+              <span>⑤ブルー×イエロー</span>
+            </li>
+          </ul>
+          </ul>
+
+          <div class="spec">
+            <dl>
+              <dt>主素材</dt>
+              <dd>①～③アンジュエール グロス<br />
+              ④⑤アンジュエール タフプラス</dd>
+            </dl>
+            <dl>
+              <dt>サイズ(内寸)</dt>
+              <dd>約W23.5×H31×D12.5cm</dd>
+            </dl>
+            <dl>
+              <dt>重量</dt>
+              <dd>約1,230g</dd>
+            </dl>
+            <dl>
+              <dt>背カン</dt>
+              <dd><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/hane.svg" /></dd>
+            </dl>
+          </div>
+        </section>
+
+        <section class="contents-brown">
+          <h2>PUMA プレミアムエディション</h2>
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/dot-mini.svg" class="dot" />
+          <p>サッカースタジアムの迫力をイメージした躍動感のあるモデル<br />
+          アクセントカラーがブラックのランドセルをより際立たせるデザインに</p>
+          <span class="price">80,300円</span>
+          <div class="slider-wrap">
+            <div id="slider5">
+              <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/puma2-img-01.png" alt="puma" /></div>
+              <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/puma2-img-02.png" alt="puma" /></div>
+              <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/puma2-img-03.png" alt="puma" /></div>
+              <div><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/puma2-img-04.png" alt="puma" /></div>
+            </div>
+
+            <div id="bx-pager5" class="slider-thumb">
+              <a data-slide-index="0" href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/puma2-img-01.png" alt="puma" /></a>
+              <a data-slide-index="1" href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/puma2-img-02.png" alt="puma" /></a>
+              <a data-slide-index="2" href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/puma2-img-03.png" alt="puma" /></a>
+              <a data-slide-index="3" href=""><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/puma2-img-04.png" alt="puma" /></a>
+            </div>
+          </div>
+
+          <h3>カラーバリエーション</h3>
+
+          <ul class="color-vali">
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike-1601-color-01.svg" />
+              <span>①ブラック×イエローグリーン</span>
+            </li>
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike-1601-color-02.svg" />
+              <span>②ブラック×カーマインレッド</span>
+            </li>
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike-1601-color-03.svg" />
+              <span>③ブラック×マリンブルー</span>
+            </li>
+            <li>
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/nike-1601-color-04.svg" />
+              <span>④ブラック×ゴールド</span>
+            </li>
+          </ul>
+          </ul>
+
+          <div class="spec">
+            <dl>
+              <dt>主素材</dt>
+              <dd>クラリーノ<small>®</small>エフ<br />
+              「レインガード<small>®</small>Fα」</dd>
+            </dl>
+            <dl>
+              <dt>サイズ(内寸)</dt>
+              <dd>約W23.5×H31×D12.5cm</dd>
+            </dl>
+            <dl>
+              <dt>重量</dt>
+              <dd>約1,200g</dd>
+            </dl>
+            <dl>
+              <dt>背カン</dt>
+              <dd><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sports/hane.svg" /></dd>
+            </dl>
+          </div>
+        </section>
+      </div>
+    </div>
+
+  <script>
+    $(document).ready(function(){
+      $('.tab-menu a').click(function(e){
+        e.preventDefault();
+        
+        var tabId = $(this).attr('href');
+        
+        $('.tab-menu li').removeClass('active');
+        $(this).parent().addClass('active');
+        
+        $('.tab-item').removeClass('active');
+        $(tabId).addClass('active');
+
+        slider.reloadSlider();
+        slider2.reloadSlider();
+        slider3.reloadSlider();
+        slider4.reloadSlider();
+        slider5.reloadSlider();
+      });
+
+      const slider = $('#slider').bxSlider({
+        pagerCustom: '#bx-pager',
+        auto: true,
+      });
+
+      const slider2 = $('#slider2').bxSlider({
+        pagerCustom: '#bx-pager2',
+        auto: true,
+      });
+
+      const slider3 = $('#slider3').bxSlider({
+        pagerCustom: '#bx-pager3',
+        auto: true,
+      });
+
+      const slider4 = $('#slider4').bxSlider({
+        pagerCustom: '#bx-pager4',
+        auto: true,
+      });
+
+      const slider5 = $('#slider5').bxSlider({
+        pagerCustom: '#bx-pager5',
+        auto: true,
+      });
+    });
+  </script>
+
   </section>
 </span>
 
