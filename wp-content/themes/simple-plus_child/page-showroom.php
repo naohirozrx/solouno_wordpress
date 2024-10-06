8448afc734fced3330d3db043ad3b2094945763a
Naohiro Fukumoto <naohirozrx@yahoo.co.jp>
2024-10-07 02:13:27 +0900

    showroom削除

--- wp-content/themes/simple-plus_child/page-showroom.php
+++ wp-content/themes/simple-plus_child/page-showroom.php
@@ -10,11 +10,30 @@
   </section>
   <ul class="showroom-list">
     <li>〇<a href="#showroom">SOLO UNO SHOWROOM</a></li>
-    <li>〇<a href="#osaka">取扱店　シブヤ大阪ショールーム</a></li>
     <li>〇<a href="#nagoya">取扱店　シブヤランドセル名古屋店</a></li>
+    <li>〇<a href="#osaka">取扱店　シブヤ大阪ショールーム</a></li>
   </ul>
   <section class="showroom-grandopen" id="showroom">
     <h2>SHOWROOM</h2>
+    <h3>店舗統合による移転のお知らせ</h3>
+    <p>平素より当店をご利用いただきまして、誠にありがとうございます。<br />
+    この度、誠に勝手ながら2024年10月20日(日)を持ちましてSOLO UNOはシブヤランドセル名古屋店と統合いたします。<br />
+    日頃よりご利用いただいておりましたお客様のご愛顧に厚く御礼申し上げます。</p>
+    <p>シブヤランドセル名古屋店<br />
+    〒458-0003<br />
+    愛知県名古屋市緑区黒沢台4-1511　鳴海プラザ1F D-1</p>
+    <p style="text-align:center;">*************</p>
+    <p>【 ランドセルに関するお問い合わせ 】<br />
+    2025モデルをお申し込みみなさまのランドセルの製作は、各メーカーにて問題なく進んでおります。</p>
+    <p>本社、㈱シブヤより発送いたしますので、ご安心ください。</p>
+    <p>また、修理やメンテナンス＆クリーニングチケットご利用の際は本社にご連絡ください。</p>
+    <p>【 ご注文のランドセルに関するお問い合わせ 】<br />
+    ㈱シブヤ　本社<br />
+    〒533-0032<br />
+    大阪府大阪市東淀川区淡路4-14-15　シブヤビル3F<br />
+    TEL：06-6326-0300　　月～金　9:00～18:00</p>
+
+    <p style="color:#dd0000; font-weight: bold;">ソロウーノ埼玉大宮店は10/20まで</p>
     <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/showroom-img.jpg" />
     <div>
       <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/map.png" />
@@ -24,6 +43,7 @@
       </span>
       <address>〒330-0854<br />埼玉県さいたま市大宮区桜木町1-9-1 三谷ビル1階</address>
       <span class="tell"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tell.svg" />048-658-3900</span>
+      <span style="display:block; color:#dd0000; font-weight: bold; margin-bottom: 1em;">2025モデルランドセルの受付は終了しました。</span>
       <a href="https://goo.gl/maps/eoRcmGMziU8eKetU8" target="_blank">Google Mapsを開く</a>
     </div>
   </section>
@@ -34,6 +54,31 @@
     <p>オーダーメイドランドセルのサンプル、
     <br />パーツサンプルを展示しています。</p>
   </header>
+
+  <section class="showroom-grandopen" id="nagoya">
+    <h2>シブヤランドセル名古屋店／SOLO UNO</h2>
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/shibuya-nagoya-img.jpg" />
+    <div>
+      <span class="open-close">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/clock.svg" />
+        <span>
+          <span style="display:block; margin-bottom:4px; line-height:1.3;">■4月～9月<br />月・金・土・日・祝のみ営業　10:00～17:00</span>
+          <span style="line-height:1.3;">■10月～3月<br />土・日・祝のみ営業　10:00～17:00</span>
+        </span>
+      </span>
+      <address>〒458-0003<br />愛知県名古屋市緑区黒沢台4-1511鳴海プラザ1F D-1</address>
+      <span class="tell"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tell.svg" />052-879-3880</span>
+
+      <span class="moyori">【名古屋市営地下鉄桜通線・徳重駅より徒歩5分】<br />
+      神沢駅より徒歩3分<br />
+      <br />
+      ※駐車場/19台分</span>
+
+      <span style="display:block; color:#dd0000; font-weight: bold; margin-bottom: 1em;">※SOLO UNOオーダーメイドランドセルの取り扱いは2026モデルからとなります。2025年3月4月ごろを予定しています。</span>
+
+      <a href="https://maps.app.goo.gl/S2neYdb211bC3pLKA" target="_blank">Google Mapsを開く</a>
+    </div>
+  </section>
 
   <section class="showroom-grandopen" id="osaka">
     <h2><span>SOLO UNO姉妹店</span>シブヤ大阪ショールーム</h2>
@@ -52,28 +97,6 @@
       　第２駐車場（淡路4-32-20）/２台分
       </span>
       <a href="https://maps.app.goo.gl/6XpGHN4oHbjqaP4W8" target="_blank">Google Mapsを開く</a>
-    </div>
-  </section>
-
-  <section class="showroom-grandopen" id="nagoya">
-    <h2><span>SOLO UNO姉妹店</span>シブヤランドセル名古屋店</h2>
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/shibuya-nagoya-img.jpg" />
-    <div>
-      <span class="open-close">
-        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/clock.svg" />
-        <span>
-          <span style="display:block; margin-bottom:4px; line-height:1.3;">■4月～9月<br />月・金・土・日・祝のみ営業　10:00～17:00</span>
-          <span style="line-height:1.3;">■10月～3月<br />土・日・祝のみ営業　10:00～17:00</span>
-        </span>
-      </span>
-      <address>〒458-0003<br />愛知県名古屋市緑区黒沢台4-1511鳴海プラザ1F D-1</address>
-      <span class="tell"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tell.svg" />052-879-3880</span>
-
-      <span class="moyori">【名古屋市営地下鉄桜通線・徳重駅より徒歩5分】<br />
-      神沢駅より徒歩3分<br />
-      <br />
-      ※駐車場/19台分</span>
-      <a href="https://maps.app.goo.gl/S2neYdb211bC3pLKA" target="_blank">Google Mapsを開く</a>
     </div>
   </section>
 </div>
